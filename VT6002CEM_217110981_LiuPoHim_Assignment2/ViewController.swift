74afb924531974ae5fe333ddb7c83c08e8179fca
LiuPoHim <him2077@gmail.com>
2022-01-04 23:57:21 +0800

    Initial Commit

---
+++ VT6002CEM_217110981_LiuPoHim_Assignment2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VT6002CEM_217110981_LiuPoHim_Assignment2
+//
+//  Created by user211668 on 1/4/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
